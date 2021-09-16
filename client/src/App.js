f289c5c65ff5322f26e47e6de859eebd825e91e0
Or Bar <zokene@gmail.com>
2021-09-16 13:37:59 +0300

    hello

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import React from 'react';
+import Header from './componenets/Header';
+import Balance from './componenets/Balance';
+import IncomeExpenses from './componenets/IncomeExpenses';
+import TransactionList from './componenets/TransactionList';
+import AddTransaction from './componenets/AddTransaction';
+
+import { GlobalProvider } from './context/GlobalState';
+
+function App() {
+  return (
+    <GlobalProvider>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </GlobalProvider>
+  );
+}
+
+export default App;
+
---
+++ client/src/componenets/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <h2>
+            Personal Costs Tracker
+        </h2>
+    )
+}
+
+export default Header;
